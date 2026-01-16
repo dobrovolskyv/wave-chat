b7f6269344124cb2da5df15a361bafc6588b70ce
Vlados <malak3@mail.ru>
2026-01-16 17:47:28 +0300

    fix posts edit

--- resources/js/Pages/Admin/Post/Index.vue
+++ resources/js/Pages/Admin/Post/Index.vue
@@ -32,7 +32,6 @@
                 <div class="flex items-center gap-8">
                     {{ post.views }}
                     {{ post.published_at }}
-                    
                 </div>
             </div>
             <div class="flex items-center gap-2">
